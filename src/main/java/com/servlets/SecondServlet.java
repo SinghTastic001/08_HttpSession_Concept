60294e9651566bbf26206a742022f1d9842720b1
SinghTastic001 <shubhisingh14034@gmail.com>
2024-09-24 23:39:16 +0530

    Initial Commit

---
+++ src/main/java/com/servlets/SecondServlet.java
@@ -0,0 +1,31 @@
+package com.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/servlet2")
+public class SecondServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		
+		HttpSession session = req.getSession();
+		String res = (String) session.getAttribute("username");
+		
+		out.println("<h1>Name "+res+"</h1>");
+	}
+	
+	
+	
+	
+
+}
